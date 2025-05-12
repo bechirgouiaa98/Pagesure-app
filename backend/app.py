ff8e0bee654845aac4196484c2d050900e165f37
bechir gouiaa <72599745+bechirgouiaa98@users.noreply.github.com>
2025-05-12 17:34:47 +0100

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -11,7 +11,7 @@ TASK_ID = "bechir.gouiaa98/facebook-scraper-task"
 
 def scrape_facebook_data(url):
     client = ApifyClient(APIFY_TOKEN)
-    run = client.task(TASK_ID).call({"startUrls": [{"url": url}]})
+    run = client.task(TASK_ID).call(input={"startUrls": [{"url": url}]})
     items = list(client.dataset(run["defaultDatasetId"]).iterate_items())
     if not items:
         return {"error": "No data found"}
